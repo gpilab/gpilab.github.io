80cbca5b8c1c75b9d7c391d97bbe1af297b85ed7
nick <nick.zwart@gmail.com>
2016-04-04 23:44:46 -0700

    Changed url format to match the old site.

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,8 +12,8 @@ STATIC_PATHS = ["images", "static"]
 
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
-ARTICLE_URL = '{date:%Y}/{date:%b}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}/index.html'
 
 PATH = 'content'
 TIMEZONE = 'America/Phoenix'
