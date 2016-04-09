89fbf3dbb6e6f935323f59dd624c5205d29ff97f
nick <nick.zwart@gmail.com>
2016-04-08 23:31:54 -0700

    added help

--- tools/reduce_jpg.sh
+++ tools/reduce_jpg.sh
@@ -1,4 +1,57 @@
 #!/bin/bash
-identify $1
-convert $1 -define jpeg:extent=${2}kb $1
-identify $1
+help ()
+{
+    echo " "
+    echo "usage: $0 <path-to-image-file> <number-of-kB>"
+    echo "    -h    this help"
+    echo " "
+    echo "    Example: $0 ~/hello.jpg 40"
+    echo "     Will convert hello.jpg into a jpeg that is less than or equal to"
+    echo "     40kB in size."
+    echo " "
+    echo "This script used ImageMagick, so any type that can be converted to"
+    echo " jpg is a valid input"
+    echo " "
+    exit 1
+}
+
+# user options
+while getopts "h" opt; do
+  case $opt in
+    h)
+      help >&2
+      ;;
+  esac
+done
+
+# check for available commands
+if command -v convert >/dev/null 2>&1; then
+    CVT="convert "
+else
+    echo "This script requires ImageMagick\'s \'convert\', aborting."
+    exit 1
+fi
+
+if command -v identify >/dev/null 2>&1; then
+    IDENT="identify"
+else
+    echo "This script requires ImageMagick\'s \'identify\', aborting."
+    exit 1
+fi
+
+# get file path and output image size
+shift $(($OPTIND - 1))
+IMAGE=$1
+SIZE=$2
+
+# make sure these args are present
+if [ -z "$IMAGE" ]; then
+    help
+fi
+if [ -z "$SIZE" ]; then
+    help
+fi
+
+$IDENT $IMAGE
+$CVT $IMAGE -define jpeg:extent=${SIZE}kb $IMAGE
+$IDENT $IMAGE
